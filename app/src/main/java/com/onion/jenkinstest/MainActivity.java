b1cec656de3d89a23ced0251ee11d479688e498d
张琦 <759308541@qq.com>
2018-04-10 13:46:08 +0800

    jenkins配置

--- app/src/main/java/com/onion/jenkinstest/MainActivity.java
+++ app/src/main/java/com/onion/jenkinstest/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     //网络地址
     public static final String NET_ADDRESS = BuildConfig.API_HOST;
+    public static final String productFlavors = BuildConfig.FLAVOR;
+    public static final boolean log = BuildConfig.DEBUG;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,9 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextView net = findViewById(R.id.net);
         TextView name = findViewById(R.id.name);
+        TextView fl = findViewById(R.id.fl);
+        TextView log = findViewById(R.id.log);
 
         net.setText("网络地址:"+NET_ADDRESS);
         name.setText("app名称:"+Utils.getApplicationName(this));
+        fl.setText("app渠道:"+productFlavors);
+        log.setText("是否开启日志:"+log);
     }
 
 
